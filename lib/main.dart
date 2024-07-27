863988e1bea5a90e54368ac92c40cc36cc0f7f12
MOKARM <mokarmdev@gmail.com>
2024-07-27 17:33:00 +0300

    change title app in main file

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Food App',
+      title: 'Our Food App',
       debugShowCheckedModeBanner: false,
       home: SignInScreen(),
     );
